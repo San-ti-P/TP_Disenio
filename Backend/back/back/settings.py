a88b371006fbf1a7493fcae43807e5fadd100f3d
San-ti-P <paggisantino@gmail.com>
2024-10-28 22:51:41 -0300

    CORS headers configurado para permitir recepción de requests desde el puerto 3000

--- Backend/back/back/settings.py
+++ Backend/back/back/settings.py
@@ -53,12 +53,16 @@ INSTALLED_APPS = BASE_APPS+LOCAL_APPS+THIRD_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 CORS_ORIGIN_WHITELIST = [
