8c0011a8e00e85fba1824326ab6f2813ef3ed4aa
San-ti-P <paggisantino@gmail.com>
2024-11-28 23:02:58 -0300

    Script para llenado de base de datos

---
+++ Backend/back/script_bdd.sql
@@ -0,0 +1,16 @@
+INSERT INTO public."Usuario" (id_usuario, contrasenia, nombre, apellido, activo, fecha_baja) VALUES ('utn-000000', 'admin1&Pass', 'Marcos', 'Debona', true, NULL);
+INSERT INTO public."Usuario" (id_usuario, contrasenia, nombre, apellido, activo, fecha_baja) VALUES ('utn-000001', 'admin2&Pass', 'Gonzalo', 'Gaitan', true, NULL);
+INSERT INTO public."Usuario" (id_usuario, contrasenia, nombre, apellido, activo, fecha_baja) VALUES ('utn-111111', 'pass#Bedel1', 'Gustavo', 'Flores', true, NULL);
+INSERT INTO public."Usuario" (id_usuario, contrasenia, nombre, apellido, activo, fecha_baja) VALUES ('utn-222222', 'pass#Bedel2', 'Ramiro', 'Arrizabalaga', true, NULL);
+INSERT INTO public."Usuario" (id_usuario, contrasenia, nombre, apellido, activo, fecha_baja) VALUES ('utn-333333', 'pass#Bedel3', 'Laura', 'Cristante', true, NULL);
+INSERT INTO public."Usuario" (id_usuario, contrasenia, nombre, apellido, activo, fecha_baja) VALUES ('utn-444444', 'pass#Bedel4', 'Carlos', 'Sosa', true, NULL);
+INSERT INTO public."Usuario" (id_usuario, contrasenia, nombre, apellido, activo, fecha_baja) VALUES ('utn-555555', 'pass#Bedel5', 'Darío', 'Carvajal', true, NULL);
+
+INSERT INTO public."Administrador" (id_usuario) VALUES ('utn-000000');
+INSERT INTO public."Administrador" (id_usuario) VALUES ('utn-000001');
+
+INSERT INTO public."Bedel" (id_usuario, turno) VALUES ('utn-111111', 'Maniana');
+INSERT INTO public."Bedel" (id_usuario, turno) VALUES ('utn-222222', 'Tarde');
+INSERT INTO public."Bedel" (id_usuario, turno) VALUES ('utn-333333', 'Noche');
+INSERT INTO public."Bedel" (id_usuario, turno) VALUES ('utn-444444', 'Maniana');
+INSERT INTO public."Bedel" (id_usuario, turno) VALUES ('utn-555555', 'Tarde');
