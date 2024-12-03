7be81296c7be61e9529ac802fef30a66e3c3be77
San-ti-P <paggisantino@gmail.com>
2024-12-03 18:16:06 -0300

    nueva bdd

--- Backend/back/script_bdd.sql
+++ Backend/back/script_bdd.sql
@@ -1,36 +1,19 @@
+TRUNCATE public."Usuario", public."Bedel", public."Administrador" CASCADE;
+
 INSERT INTO public."Usuario" (id_usuario, contrasenia, nombre, apellido, activo, fecha_baja) VALUES 
-('utn-000000', 'admin1&Pass', 'Marcos', 'Debona', true, NULL),
-('utn-000001', 'admin2&Pass', 'Gonzalo', 'Gaitan', true, NULL),
-('utn-111111', 'pass#Bedel1', 'Gustavo', 'Flores', true, NULL),
-('utn-222222', 'pass#Bedel2', 'Ramiro', 'Arrizabalaga', true, NULL),
-('utn-333333', 'pass#Bedel3', 'Laura', 'Cristante', true, NULL),
-('utn-444444', 'pass#Bedel4', 'Carlos', 'Sosa', true, NULL),
-('utn-555555', 'pass#Bedel5', 'Darío', 'Carvajal', true, NULL),
-('utn-666666', 'pass#Bedel6', 'Ana', 'Martinez', true, NULL),
-('utn-777777', 'pass#Bedel7', 'Pedro', 'Gomez', true, NULL),
-('utn-888888', 'pass#Bedel8', 'Lucia', 'Perez', true, NULL),
-('utn-999999', 'pass#Bedel9', 'Juan', 'Lopez', true, NULL),
-('utn-101010', 'pass#Bedel10', 'Sofia', 'Garcia', true, NULL),
-('utn-119111', 'pass#Bedel11', 'Miguel', 'Rodriguez', true, NULL),
-('utn-121212', 'pass#Bedel12', 'Marta', 'Fernandez', true, NULL),
-('utn-131313', 'pass#Bedel13', 'Jose', 'Martinez', true, NULL),
-('utn-141414', 'pass#Bedel14', 'Maria', 'Gonzalez', true, NULL),
-('utn-151515', 'pass#Bedel15', 'Luis', 'Hernandez', true, NULL),
-('utn-161616', 'pass#Bedel16', 'Elena', 'Diaz', true, NULL),
-('utn-171717', 'pass#Bedel17', 'Carlos', 'Ruiz', true, NULL),
-('utn-181818', 'pass#Bedel18', 'Laura', 'Moreno', true, NULL),
-('utn-191919', 'pass#Bedel19', 'Jorge', 'Muñoz', true, NULL),
-('utn-202020', 'pass#Bedel20', 'Clara', 'Romero', true, NULL),
-('utn-212121', 'pass#Bedel21', 'Raul', 'Alvarez', true, NULL),
-('utn-292222', 'pass#Bedel22', 'Carmen', 'Gutierrez', true, NULL),
-('utn-232323', 'pass#Bedel23', 'Alberto', 'Sanchez', true, NULL),
-('utn-242424', 'pass#Bedel24', 'Isabel', 'Ortiz', true, NULL),
-('utn-252525', 'pass#Bedel25', 'Victor', 'Iglesias', true, NULL),
-('utn-262626', 'pass#Bedel26', 'Rosa', 'Castro', true, NULL),
-('utn-272727', 'pass#Bedel27', 'Francisco', 'Vargas', true, NULL),
-('utn-282828', 'pass#Bedel28', 'Patricia', 'Ramos', true, NULL),
-('utn-292929', 'pass#Bedel29', 'Manuel', 'Gil', true, NULL),
-('utn-303030', 'pass#Bedel30', 'Teresa', 'Molina', true, NULL);
+('utn-000000', '$2b$12$AGJJjVaYiowu/07AGLEVtOaxijElrlKC0JWnxzPmARm8t.1qB.1Uu', 'Marcos', 'Debona', true, NULL),
+('utn-000001', '$2b$12$.lnzsK8emAnoLZtx6ORh/Omvx71ZHkfaxnTKXC7.1dX3sKdej9PgW', 'Gonzalo', 'Gaitan', true, NULL),
+('utn-111111', '$2b$12$J89rWnokcsCohybni/dIfuGUy3OHaNsktdusWvxSeAvfdCfwA2Dea', 'Gustavo', 'Flores', true, NULL),
+('utn-222222', '$2b$12$HCCSuOju9LUd4.aJKeiNbOPqTDawl2n12VKheJgtPATYyoWDkkfS2', 'Ramiro', 'Arrizabalaga', true, NULL),
+('utn-333333', '$2b$12$MAW/vQIItlUlNIRwdWRU3uXvM0eTnp9fFGUxXhvZKK0YEmBfbu1Eq', 'Laura', 'Cristante', true, NULL),
+('utn-444444', '$2b$12$7zai9wdKrBBPQ1.3wQ2IUOIeU9x6bj5Uhl4ZBc0k7KQ/4cJEyQ4cC', 'Carlos', 'Sosa', true, NULL),
+('utn-555555', '$2b$12$fAGHnAjT9IxX2oHC398QdOspH4KsdLDg1LQmHv0edeuwZ3NmWn4ou', 'Darío', 'Carvajal', true, NULL),
+('utn-666666', '$2b$12$HbhyA5LQEU.igQ7ZSH2pberIo1EmtC8rOz0vzQN6P5uR28bAJKy6m', 'Ana', 'Martinez', true, NULL),
+('utn-777777', '$2b$12$18dG/3V5XajSyEQR8lpfIuOsixMgShILKvHjOlP02mUrmE7aJW0R.', 'Pedro', 'Gomez', true, NULL),
+('utn-888888', '$2b$12$7V2IhGvQS.SIH9aBQWZW7exv7Ur3AQcpWRilIhZykxfZczq14cGwO', 'Lucia', 'Perez', true, NULL),
+('utn-999999', '$2b$12$seQm4nUM2KkUYcopuSiQvOP8dHaMh8QGp/r8agiM5qD1tOwzxxjbK', 'Juan', 'Lopez', true, NULL),
+('utn-101010', '$2b$12$QObdc0hk7iazkzsfzJzEZ.WF3WtNI4VfYNr.s8lvro947wPCjsStK', 'Sofia', 'Garcia', true, NULL);
+
 
 INSERT INTO public."Administrador" (id_usuario) VALUES 
 ('utn-000000'),
@@ -46,24 +29,17 @@ INSERT INTO public."Bedel" (id_usuario, turno) VALUES
 ('utn-777777', 'Maniana'),
 ('utn-888888', 'Tarde'),
 ('utn-999999', 'Noche'),
-('utn-101010', 'Maniana'),
-('utn-119111', 'Tarde'),
-('utn-121212', 'Noche'),
-('utn-131313', 'Maniana'),
-('utn-141414', 'Tarde'),
-('utn-151515', 'Noche'),
-('utn-161616', 'Maniana'),
-('utn-171717', 'Tarde'),
-('utn-181818', 'Noche'),
-('utn-191919', 'Maniana'),
-('utn-202020', 'Tarde'),
-('utn-212121', 'Noche'),
-('utn-292222', 'Maniana'),
-('utn-232323', 'Tarde'),
-('utn-242424', 'Noche'),
-('utn-252525', 'Maniana'),
-('utn-262626', 'Tarde'),
-('utn-272727', 'Noche'),
-('utn-282828', 'Maniana'),
-('utn-292929', 'Tarde'),
-('utn-303030', 'Noche');
+('utn-101010', 'Maniana');
+
+--{'utn-000000', 'admin1&Pass'}
+--{'utn-000001', 'admin2&Pass'}
+--{'utn-111111', 'pass#Bedel1'} 
+--{'utn-222222', 'pass#Bedel2'}
+--{'utn-333333', 'pass#Bedel3'}
+--{'utn-444444', 'pass#Bedel4'}
+--{'utn-555555', 'pass#Bedel5'}
+--{'utn-666666', 'pass#Bedel6'}
+--{'utn-777777', 'pass#Bedel7'}
+--{'utn-888888', 'pass#Bedel8'}
+--{'utn-999999', 'pass#Bedel9'}
+--{'utn-101010', 'pass#Bedel10'}
