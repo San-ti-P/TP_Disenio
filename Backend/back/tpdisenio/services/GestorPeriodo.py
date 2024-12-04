1c4349c88c2ead52cd72f0ff269b1af2839c6895
Kevinbrunas <kevinbrunas@hotmail.com>
2024-12-04 14:52:27 -0300

    algo mas

---
+++ Backend/back/tpdisenio/services/GestorPeriodo.py
@@ -0,0 +1,12 @@
+from ..models import Periodo
+
+class GestorPeriodo():
+    def __init__(self, periodo_DAO) -> None:
+        
+        self.periodo_DAO = periodo_DAO
+
+    def alta_periodo(self, tipo, anio): 
+        pass
+
+    def get_periodo(self, tipo, anio): 
+        pass
