e5491dbb2ebc6ad0bfae154eca0ebd32d93c4685
gonzag8 <gonzagaitan8@gmail.com>
2024-12-06 17:39:18 -0300

    Avances CU 1, se perfila para el final

--- Frontend/frontend/src/componentes/calendarioEsporadico.js
+++ Frontend/frontend/src/componentes/calendarioEsporadico.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import 'react-calendar/dist/Calendar.css';
-import {CalendarioEstilado} from "../elementos/calendario"
+import { CalendarioEstilado } from "../elementos/calendario";
 
 const CalendarioPersonalizado = ({ onClickDay, tileClassName, value }) => {
+  const deshabilitarDiasAnteriores = ({ date }) => {
+    const hoy = new Date();
+    hoy.setHours(0, 0, 0, 0); // Establece la hora a las 00:00:00 para comparar solo la fecha
+    return date < hoy;
+  };
+
   return (
     <CalendarioEstilado
       onClickDay={onClickDay}
@@ -10,6 +16,7 @@ const CalendarioPersonalizado = ({ onClickDay, tileClassName, value }) => {
       value={value}
       locale="es-ES"
       formatShortWeekday={(locale, date) => ['D', 'L', 'M', 'M', 'J', 'V', 'S'][date.getDay()]}
+      tileDisabled={deshabilitarDiasAnteriores}
     />
   );
 };
